e3ffb28e632e19bd6bca34568c0627a196f4fba8
silviaysy <584583599@qq.com>
2020-07-11 20:50:30 +0800

    update alpha_101.py and utils.py

--- alpha_research/factor_zoo/test.py
+++ alpha_research/factor_zoo/test.py
@@ -1,10 +1,14 @@
 from alpha_research.factor_zoo.utils import *
+from alpha_research.factor_zoo.alpha_101 import *
 import pandas as pd
-data = pd.read_csv(r'../../hsi_component.csv')
-data['Date'] = pd.to_datetime(data['Date'])
-data.set_index(['Date', 'code'], inplace=True)
-close_ = data['close']
-volume_ = data['volume']
-open_ = data['open']
+if __name__ == '__main__':
+    data = pd.read_csv(r'../../hsi_component.csv')
 
+    data['Date'] = pd.to_datetime(data['Date'])
+    data.set_index(['Date', 'code'], inplace=True)
+    # print(data)
+    # close_ = data['close']
+    # volume_ = data['volume']
+    # open_ = data['open']
+    print(alpha_4(data))
 
